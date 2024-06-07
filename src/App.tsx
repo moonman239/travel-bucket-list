159a4b1f47cd25311881f82e551b98e8a4122952
montana.linux <montana.burr@gmail.com>
2024-06-07 13:37:13 -0600

    added listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 export default function App()
 {
+  const [location,setLocation] = React.useState<string>("");
+  const [locations, setLocations] = React.useState<string[]>([]); // [1]
+
   const handleSubmit = async (e: React.FormEvent) => {
-    alert("submission");
+e.preventDefault();
+    setLocations([...locations, location]);
+
+
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // add location to state
+    setLocation(e.target.value);
   };
   return (
     <div className="container">
         <h1>Travel Bucket List</h1>
         <form id="locationForm" onSubmit={handleSubmit}>
-            <input type="text" id="locationInput" placeholder="Enter location" required />
+            <input type="text" onChange={handleChange} id="locationInput" placeholder="Enter location" required />
             <button type="submit">Add Location</button>
         </form>
-        <ul id="bucketList"></ul>
+        <ul id="bucketList">{locations.map((l)=><li>{l}</li>)}</ul>
     </div>
   );
 }
